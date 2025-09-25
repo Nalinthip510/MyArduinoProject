9af978f23248fe763dea69a691765ea66ea74b0d
Nalinthip510 <s6503014612043@email.kmutnb.ac.th>
2025-09-25 11:05:32 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int ledPin = 13; // LED บนบอร์ด Arduino Uno
+
+void setup() {
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED ON");
+    delay(1000);
+
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED OFF");
+    delay(1000);
+}
